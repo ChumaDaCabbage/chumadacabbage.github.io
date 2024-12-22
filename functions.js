56d87fdd74bfab19a5c73b44f1666cc9dcc18d0b
ChumaDaCabbage <ChumaDaCabbage@users.noreply.github.com>
2024-12-21 20:48:39 -0800

    Added icons to contact info and contact info heighlight

--- functions.js
+++ functions.js
@@ -2,4 +2,18 @@ function scrollToFooter()
 {
   const footerElement = document.querySelector('.footer');
   footerElement.scrollIntoView({ behavior: 'smooth' });
+  highlightText()
+}
+
+function highlightText() 
+{
+  const textElements = document.querySelectorAll('.footer .contactInfo .text');
+  
+  textElements.forEach((textElement) => 
+  {
+    textElement.classList.add('highlight');
+    
+    //Remove the highlight class after a delay (1 second)
+    setTimeout(() => {textElement.classList.remove('highlight');}, 1000);
+  });
 }
