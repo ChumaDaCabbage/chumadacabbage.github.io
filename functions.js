f4e81aab9ea97074e634055f48f1b6d65d357f24
ChumaDaCabbage <ChumaDaCabbage@users.noreply.github.com>
2024-12-27 17:30:19 -0800

    Made so text zooms well, fixed button size in project page

--- functions.js
+++ functions.js
@@ -26,7 +26,8 @@ function scrollToFooter()
     {
       const code = await response.text();
       document.getElementById(filename).innerHTML = `<pre class="prettyprint lang-cpp">${code}</pre>`;
-      PR.prettyPrint();  //Run Google Prettify to style the code
+      PR.prettyPrint();  //Run Google Prettify to style the code     
+      //scrollGrower(filename);
     } 
     else 
     {
@@ -60,3 +61,60 @@ async function loadHTML(filename)
    console.error('Error loading the html:', error);
  }
 }
+
+//Set y offset based off another elements height
+function setOffsetFromHeight(sourceClass, targetClass) 
+{
+  //Get the height of the source element
+  const sourceElements = document.getElementsByClassName(sourceClass);
+  sourceElement = sourceElements[0]; //Get the first element
+  const targetElements = document.getElementsByClassName(targetClass);
+  targetElement = targetElements[0]; //Get the first element
+
+  if (sourceElement && targetElement) 
+  {
+    const sourceHeight = sourceElement.offsetHeight; //Get the height of the source
+    targetElement.style.top = `${-sourceHeight - 10}px`;   //Set the top offset of the target
+  } 
+  else 
+  {
+    console.error("One or both elements not found.");
+  }
+}
+
+// let StartHeight = -1; //Starting height of the element
+
+// //Increases size of element by how much scrolling has been done
+// function scrollGrower(elementId)
+// {
+//   //If the starting height has not been set, set it
+//   if(StartHeight === -1)
+//   {
+//     const element = document.getElementById(elementId);
+//     StartHeight = element.offsetHeight;
+
+//     //Print the starting height
+//     console.log(`Start Height: ${StartHeight}`);
+//   }
+
+//   //Start with the distance from the top of this element to the bottom of the screen
+//   const element = document.getElementById(elementId);
+//   const rect = element.getBoundingClientRect();
+//   distance = window.innerHeight - rect.top;
+
+//   //Add how much has been scrolled to the distance in the y
+//   const scrollDistance = window.scrollY;
+//   distance += scrollDistance;
+
+//   //Subtract the height of the scroll bar
+//   const scrollBarHeight = window.innerHeight - document.documentElement.clientHeight;
+//   distance -= 20;
+
+//   //Print distance scroll distance and the distance from the top of the element to the bottom of the screen
+//   console.log(`Scroll Distance: ${scrollDistance}`);
+//   console.log(`Distance: ${distance}`);
+//   console.log(`Distance: ${distance}`);
+
+//   //Set the elements height to the new distance
+//   element.style.height = `${distance}px`;
+// }
